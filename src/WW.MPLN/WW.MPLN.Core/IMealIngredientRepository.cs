e609870233b270e63b9147eee5d03c938af18d08
vicbai <vicbai@westwin.com>
2019-07-08 09:59:23 +0800

    -- IngredientController

---
+++ src/WW.MPLN/WW.MPLN.Core/IMealIngredientRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WW.MPLN.Core
+{
+    public interface IMealIngredientRepository
+    {
+    }
+}
---
+++ src/WW.MPLN/WW.MPLN.Core/MealIngredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WW.MPLN.Core
+{
+    public class MealIngredient
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string NBDID { get; set; }
+    }
+}
